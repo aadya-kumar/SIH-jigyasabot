9bdaa6dd4634bcadcb07c41cf926a77351635bbb
aadya-kumar <117282728+aadya-kumar@users.noreply.github.com>
2024-09-28 15:09:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// script.js
+document.getElementById('start-chat-btn').onclick = function() {
+    window.location.href = 'chat.html';
+};
+
+document.getElementById('doc-summary-btn').onclick = function() {
+    window.location.href = 'document-summary.html';
+};
